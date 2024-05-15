ccac5a83b105d2ee24b3911fdce55e7f6c2d92a0
qwertyuiopasdfghjklzxcvbnm1234567890000 <159198097+qwertyuiopasdfghjklzxcvbnm1234567890000@users.noreply.github.com>
2024-05-15 10:18:47 -0700

    set up shooting mechanics - complete

--- gun.gd
+++ gun.gd
@@ -11,4 +11,10 @@ func _physics_process(delta):
 func shoot_sperm_cells():
 	const BULLET = preload("res://bullet.tscn")
 	var new_bullet = BULLET.instantiate()
-	new_bullet.global_position = %ShootingPoint
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout():
+	shoot_sperm_cells()
