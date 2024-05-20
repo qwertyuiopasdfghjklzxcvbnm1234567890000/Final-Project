6cdda905656dab433f638b18277fba8f6c666bd5
qwertyuiopasdfghjklzxcvbnm1234567890000 <159198097+qwertyuiopasdfghjklzxcvbnm1234567890000@users.noreply.github.com>
2024-05-20 15:43:21 -0700

    created dynamic blood overlay animation

---
+++ dynamic_blood_overlay.gd
@@ -0,0 +1,3 @@
+extends CanvasLayer
+func _ready():
+		$TextureRect/AnimationPlayer.play("flash")
--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 signal health_delpleted
 
+
 var health = 100.0
 
 func _physics_process(delta):
@@ -17,8 +18,8 @@ func _physics_process(delta):
 	const DAMAGE_RATE = 5.0
 	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
+		preload("res://dynamic_blood_overlay.tscn").instantiate()
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
-		
 		%ProgressBar.value = health
 		if health <= 0.0:
 			health_delpleted.emit()
