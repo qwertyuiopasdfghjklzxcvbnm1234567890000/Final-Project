bdf6b5a79921468fb16318f602eb4cc475a9edf1
qwertyuiopasdfghjklzxcvbnm1234567890000 <159198097+qwertyuiopasdfghjklzxcvbnm1234567890000@users.noreply.github.com>
2024-05-22 11:05:16 -0700

    misc

--- old_man.gd
+++ old_man.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
-func sacrifice():
+func sacrifice_old_man():
 	if Input.is_action_just_pressed("spawn_old_man"):
 		show()
+		mutilate_old_man()
+func mutilate_old_man(): 
+	get_node("")
